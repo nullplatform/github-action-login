98536b69178bcf6be2ddb21a3b00852080106f65
Ricardo Q. Bazan <raquirozbazan@gmail.com>
2025-10-20 13:58:30 -0500

    wip

--- src/client.js
+++ src/client.js
@@ -3,12 +3,11 @@ const config = require('./config');
 
 class HttpClient {
   constructor() {
-    this.client = new http.HttpClient('np-gh-action-login', [], {
-      allowRetries: true,
-      maxRetries: 3,
-    });
+    this.client = new http.HttpClient();
     this.client.requestOptions = {
       headers: { [http.Headers.ContentType]: 'application/json' },
+      allowRetries: true,
+      maxRetries: 3,
     };
     this.baseUrl = config.baseUrl;
   }
